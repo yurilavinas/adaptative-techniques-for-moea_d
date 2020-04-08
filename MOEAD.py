850de90068518eb001a672f174b7f8e842835f70
yclavinas <yclavinas@gmail.com>
2020-04-08 11:19:48 +0900

    on doing code polish; priority functions add-ons

--- MOEAD.py
+++ MOEAD.py
@@ -32,10 +32,10 @@ params = yaml.safe_load(args.params)                                # read confi
 random.seed(args.seed)
 np.random.seed(args.seed)
 
-prefix = params['prefix']                                           # set prefix of record in this run
+output = params['output']                                           # set output of record in this run
 
 prob_name = params['prob_name']                                     # set optimization problem
-f = set_problem(prob_name)
+problem = set_problem(prob_name)
 n_obj = params['n_obj']                                             # set number of objectives
 n_var = params['n_var']                                             # set number of variables
 xl = params['xl']                                                   # set boundary of variables
@@ -45,7 +45,6 @@ sld_n_part = params['sld_n_part']                                           # se
 
 n_eval = params['n_eval']                                           # set maximum number of evaluation
 n_pop = int( comb(n_obj + sld_n_part - 1, n_obj - 1) )                  # compute population size
-n_gen = n_eval // n_pop                                             # compute maximum generation
 
 T = params['T']                                                     # set neighbor size
 delta = params['delta']                                             # set probability to select parent from neighbor
@@ -55,31 +54,41 @@ alpha = params['alpha']                                             # set scalin
 beta = params['beta']                                               # set stability parameter of levy flight mutation
 etam = params['etam']                                               # set index parameter of polynomial mutation
 
+priority_function = params['priority_function']                     # name of the priority function for resource allocation
+fix_value = params['fix_value']                                     # set index parameter of priority functions for resource allocation
+
 #################
 # start program #
 #################
 
-os.makedirs(f'./{prefix}', exist_ok=True)                           # create a folder to include running results
-os.makedirs(f'./{prefix}/history/', exist_ok=True)
-os.makedirs(f'./{prefix}/history/{args.seed}', exist_ok=True)
+os.makedirs(f'./{output}', exist_ok=True)                           # create a folder to include running results
+os.makedirs(f'./{output}/history/', exist_ok=True)
+os.makedirs(f'./{output}/history/{args.seed}', exist_ok=True)
 
-W = das_dennis(sld_n_part, n_obj)                                       # generate a set of weight vectors
+W = das_dennis(sld_n_part, n_obj)                                   # generate a set of weight vectors
 B = determine_neighbor(W, T)                                        # determine neighbor
 X = init_pop(n_pop, n_var, xl, xu)                                  # initialize a population
-F = eval_pop(X, f)                                                  # evaluate fitness
-z = init_ref_point(F)                                               # determine a reference point
+Y = eval_pop(X, problem)                                                  # evaluate fitness
+z = init_ref_point(Y)                                               # determine a reference point
 
-priority_values = fixed_priority_values(n_pop)
+n_fe = n_pop
 
-for c_gen in range(1, n_gen):                                       # star main loop
+priority_values = fixed_priority_values(n_pop, priority_function, fix_value) # generate an array of values for resource allocation based on priority_function named function
 
-    result = np.hstack([F, X])                                      # record objective values and decision variables
-    np.savetxt(f'./{prefix}/history/{args.seed}/{c_gen}.csv', result)
+# for c_gen in range(1, n_gen):                                       # start main loop
+c_gen = 1
+while n_fe < n_eval:
+    result = np.hstack([Y, X])                                      # record objective values and decision variables
+    np.savetxt(f'./{output}/history/{args.seed}/{c_gen}_paretos.csv', result)
 
-    for i in np.random.permutation(n_pop):                          # traverse the population
+    info_gen = np.hstack([n_fe, c_gen])                                      # record information (n_fe and c_gen) about the current generation
+    np.savetxt(f'./{output}/history/{args.seed}/{c_gen}_info_gen.csv', info_gen)
+
+    for i in np.random.permutation(n_pop):                          # traverse the population; randomly permuting solutions in the population set
 
         if (priority_values[i] >= np.random.uniform()):
-            xi, fi = X[i, :], F[i, :]                                   # get current individual and its fitness value
+            n_fe += 1
+            xi, fi = X[i, :], Y[i, :]                                   # get current individual and its fitness value
 
             if random.random() < delta:                                 # determine selection pool by probability
                 pool = B[i, :]                                          # neighbor as the pool
@@ -87,29 +96,33 @@ for c_gen in range(1, n_gen):                                       # star main 
                 pool = np.arange(n_pop)                                 # population as the pool
 
             j = np.random.choice(pool)                                  # select a random individual from pool
-            xj, fj = X[j, :], F[j, :]
+            xj, fj = X[j, :], Y[j, :]
 
             xi_ = fix_bound( lf_mutation(xi, xj, alpha, beta), xl, xu ) # levy flight mutation
             xi_ = fix_bound( poly_mutation(xi_, etam, xl, xu), xl, xu ) # polynomial mutation
-            fi_ = f(xi_)                                                # evaluate offspring
+            fi_ = problem(xi_)                                                # evaluate offspring
 
             z = update_ref_point(z, fi_)                                # update reference point
 
             nc = 0                                                      # initialize the update counter
             for k in np.random.permutation(len(pool)):                  # traverse the selection pool
 
-                fk = F[k, :]                                            # get k-th individual fitness
+                fk = Y[k, :]                                            # get k-th individual fitness
                 wk = W[k, :]                                            # get k-th weight vector
 
                 if tchebycheff(fi_, wk, z) <= tchebycheff(fk, wk, z):   # compare tchebycheff cost of offspring and parent
                     X[k] = xi_                                          # update parent
-                    F[k] = fi_
+                    Y[k] = fi_
                     nc += 1                                             # cumulate the counter
 
                 if nc >= nr: break                                      # break if counter arrive the upper limit
+    c_gen += 1
+    priority_values = fixed_priority_values(n_pop, priority_function, fix_value)
 
-    priority_values = fixed_priority_values(n_pop)
+result = np.hstack([Y, X])                                          # record objective values and decision variables
 
-result = np.hstack([F, X])                                          # record objective values and decision variables
-np.savetxt(f'./{prefix}/history/{args.seed}/{n_gen}.csv', result)
-np.savetxt(f'./{prefix}/{args.seed}_final.csv', result)
+np.savetxt(f'./{output}/history/{args.seed}/{c_gen}_paretos.csv', result)
+np.savetxt(f'./{output}/{args.seed}_final.csv', result)
+
+info_gen = np.hstack([n_fe, c_gen])                                      # record information (n_fe and c_gen) about the current generation
+np.savetxt(f'./{output}/history/{args.seed}/{c_gen}_info_gen.csv', info_gen)
--- PriorityFunctions.py
+++ PriorityFunctions.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def fixed_priority_values(n_pop, value = 0.1):
+def fixed_priority_values(n_pop, function, value = 0.1):
     """
     Initialize a population with uniform distribution
 
@@ -8,6 +8,31 @@ def fixed_priority_values(n_pop, value = 0.1):
     ----------
     n_pop: int
       population size
+    (priority) function: 
+      name of the function used for resource allocation
+    
+
+    return
+    ----------
+    1D-Array
+      an array of fixed values for resource allocation
+    """
+    if(function == "fix_random"):
+        X = fix_random(n_pop, value)
+    return X
+
+def fix_random(n_pop, value = 0.1):
+    """
+    Initialize a population with uniform distribution
+
+    parameter
+    ----------
+    n_pop: int
+      population size
+    (priority) function: 
+      name of the function used for resource allocation
+    value: float
+      fix value for selecting part of the population
     
 
     return
@@ -16,4 +41,5 @@ def fixed_priority_values(n_pop, value = 0.1):
       an array of fixed values for resource allocation
     """
     X = np.full(n_pop, value)
+
     return X
