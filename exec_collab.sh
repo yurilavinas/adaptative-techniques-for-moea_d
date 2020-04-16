df3270170b048d49c00a324849fdc1b8db1aed40
yclavinas <yclavinas@gmail.com>
2020-04-16 19:24:23 +0900

    scripts for running exp

--- exec_collab.sh
+++ exec_collab.sh
@@ -1,8 +1,15 @@
 # Usage
 # python MOEAD.py [parameter yml file] [random seed]
  
-nohup sh random.sh &
+nohup sh random_dtlz7.sh &
 
-nohup sh ds_norm.sh &
+nohup sh ds_norm_dtlz7.sh &
 
-nohup sh RI.sh & 
+nohup sh RI_dtlz7.sh & 
+
+nohup sh random_uf9.sh &
+
+nohup sh ds_norm_uf9.sh &
+
+nohup sh RI_uf9.sh & 
+
