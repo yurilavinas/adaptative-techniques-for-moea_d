7e83b699a7e7b68381f13897e25b720443fe5c32
Y1fanHE <heyf95318@yahoo.co.jp>
2020-04-23 13:33:05 +0900

    Update documentation

--- AdaptiveStrategy.py
+++ AdaptiveStrategy.py
@@ -8,19 +8,55 @@ def evolve(P, P_parent, P_offspring, I, I_parent, I_offspring, B,
 
     parameter
     ----------
-    !TODO write documentation
+    P: 1D-Array
+      values of parameters of each individual in the current population
+    P_parent: 1D-Array
+      saved values of parameters in parameter parent population
+    P_offspring: 1D-Array
+      saved values of parameters in parameter offspring population
+    I: 1D-Array
+      indicator values of parameters of each individual in the current population
+    I_parent: 1D-Array
+      saved indicator values of parameters in parameter parent population
+    I_offspring: 1D-Array
+      saved indicator values of parameters in parameter offspring population
+    B: list
+      list of neighbor index information
+    n_step: int
+      numbers of generations to update parameters
+    betal: float
+      lower bound of beta, usually 0.1
+    betau: float
+      upper bound of beta, usually 1.9
+    alpha_for_param: float
+      scaling factor of levy flight used to update parameters
+    beta_for_param: float
+      stability factor of levy flight used to update parameters
+
     return
     ----------
-    !TODO write documentation
+    P: 1D-Array
+      values of parameters of each individual in the current population
+    P_parent: 1D-Array
+      saved values of parameters in parameter parent population
+    P_offspring: 1D-Array
+      saved values of parameters in parameter offspring population
+    I: 1D-Array
+      indicator values of parameters of each individual in the current population
+    I_parent: 1D-Array
+      saved indicator values of parameters in parameter parent population
+    I_offspring: 1D-Array
+      saved indicator values of parameters in parameter offspring population
     """
     n_pop = len(P)
-    if c_gen % (2 * n_step) == n_step:
+    if c_gen % (2 * n_step) == n_step:                                      # generate offspring parameters by levy flight
         P_parent = P[:]
         I_parent = I[:]
-        P = fix_bound( P + levy(alpha_for_param, beta_for_param, n_pop), betal, betau)
+        P = fix_bound( P + levy(alpha_for_param, beta_for_param, n_pop),
+                       betal, betau)
         I = I * 0
 
-    if c_gen % (2 * n_step) == 0:
+    if c_gen % (2 * n_step) == 0:                                           # select better parameter in a parent-offspring paremeter pair
         P_offspring = P[:]
         I_offspring = I[:]
         for i in np.random.permutation(n_pop):
